76f773786687356d78acd0362ba77876fd397a60
Pratik Ganjale <pratik.ganjale@elevatetrips.com>
2026-05-22 18:13:41 +0530

    change in file routing

--- server.ts
+++ server.ts
@@ -6,7 +6,20 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("TypeScript Node.js Server Running");
+  res.send("TypeScript Node.js Server Running...");
+});
+
+app.get("/data", (req: Request, res: Response) => {
+  res.json([
+    {
+      name : "Alice",
+      age : 30
+    },
+    {
+      name : "Bob",
+      age : 25
+    }
+  ])
 });
 
 app.listen(PORT, () => {
