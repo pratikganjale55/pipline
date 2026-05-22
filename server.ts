f3fa1e46b29c58406dc6555dd5caa67e9cad4ca9
Pratik Ganjale <pratik.ganjale@elevatetrips.com>
2026-05-22 16:39:40 +0530

    create docker file

--- server.ts
+++ server.ts
@@ -1,8 +1,9 @@
+import dotenv from 'dotenv';
 import express, { Request, Response } from "express";
-
+dotenv.config();
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("TypeScript Node.js Server Running");
